7392b401ce38f654d8bda679eefa56e96b06f68c
Jacqueline Cruz <107505857+AzucenaCruz@users.noreply.github.com>
2026-05-18 15:36:59 -0600

    Delete backend directory

--- backend/Helpers/Roles.cs
+++
@@ -1,9 +0,0 @@
-namespace TelMedAPI.Helpers
-{
-    public static class Roles
-    {
-        public const string Admin = "Admin";
-        public const string Doctor = "Doctor";
-        public const string Paciente = "Paciente";
-    }
-}
