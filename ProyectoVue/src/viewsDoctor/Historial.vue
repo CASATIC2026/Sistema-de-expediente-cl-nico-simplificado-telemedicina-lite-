3e7c0c97240afdc98468a4035b3b098a909e5ece
Jacqueline Cruz <107505857+AzucenaCruz@users.noreply.github.com>
2026-05-18 15:20:05 -0600

    Delete ProyectoVue directory

--- ProyectoVue/src/viewsDoctor/Historial.vue
+++
@@ -1,8 +0,0 @@
-///SE ELIMINARÁ, PERO PREVIAMEMNTE ELIMINAR EL BOTON DE HISTORIAL DEL SIDEBAR, YA QUE NO SE USA EN NINGUNA PARTE DEL PROYECTO
-
-<template>
-  <div class="p-4">
-    <h2 class="text-2xl font-bold mb-4">Pacientes</h2>
-    <p>Esta vista mostrará el historial</p>
-  </div>
-</template>
